1c9b832e68022ca87efeb2045ad6ce27b47eae45
Anuwat Thisuka <43104719+AnuwatThisuka@users.noreply.github.com>
2025-04-26 10:55:52 +0700

    Update setup-autostart.sh

--- setup-autostart.sh
+++ setup-autostart.sh
@@ -29,10 +29,10 @@ cat > "$AUTOSTART_FILE" <<EOL
 @lxpanel --profile LXDE-pi
 @pcmanfm --desktop --profile LXDE-pi
 @xscreensaver -no-splash
+@chromium-browser --noerrdialogs --disable-infobars --kiosk $URL
 @xset s off
 @xset -dpms
 @xset s noblank
-@sh -c "sleep 10 && chromium-browser --noerrdialogs --disable-infobars --kiosk --incognito --no-first-run --disable-session-crashed-bubble --disable-features=TranslateUI --disable-contextual-search --disable-pinch --overscroll-history-navigation=0 --user-data-dir=/tmp $URL"
 EOL
 
 zenity --info --text="ตั้งค่าเรียบร้อยแล้ว!\nจะเปิด $URL แบบเต็มจอเมื่อบูตเครื่อง"
